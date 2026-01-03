11437f1841155c74429b007c7de5de885d3586f2
Krish-01-code <krishkushwaha.w@gmail.com>
2026-01-03 02:46:36 -0500

    list

---
+++ python20-list.py
@@ -0,0 +1,14 @@
+#collection = a concept where you use a single variable to store 
+# multiple values.
+
+# 1. List [] - a type of collection
+# which is ordered and changeable. Duplicates are allowed 
+
+
+fruits = ["Apple","Banana","Mango"]
+
+print(fruits[-1::-1])
+
+fruits[2] = "Orange"
+
+print(fruits[-1])
