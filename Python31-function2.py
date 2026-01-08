adf91624c5a4047b1eb55b292b04f7a7fae51d7c
Krish-01-code <krishkushwaha.w@gmail.com>
2026-01-07 22:04:37 -0500

    minor mistake

--- Python31-function2.py
+++ Python31-function2.py
@@ -7,7 +7,7 @@
 #This program will be about default arguments 
 
 
-def MassOfNucleon(P , N, p = 1 , n = 1):
+def MassOfNeucleon(P , N, p = 1 , n = 1):
     mass = P*p + N*n
     return mass
 
@@ -18,12 +18,12 @@ print("Default mass of proton and neutron is 1 amu.")
 choice = input("Do you want to change it(Y/N)? ")
 
 if choice == "Y":
-    mass_proton = (print("Enter the mass of proton in amu: "))
-    mass_neutron = (print("ENter the mass of neutron in amu: "))
+    mass_proton = float(input("Enter the mass of proton in amu: "))
+    mass_neutron = float(input("ENter the mass of neutron in amu: "))
 
-    Total_mass = MassOfNucleon(Num_proton,Num_neutron,mass_proton,mass_neutron)
-    print(f"The total mass of neucleon is: {Total_mass} amu")
+    Total_mass = MassOfNeucleon(Num_proton,Num_neutron,mass_proton,mass_neutron)
+    print(f"The total mass of neucleon is: {Total_mass:02f} amu")
 
 else:
-    Total_mass = MassOfNucleon(Num_proton,Num_neutron)
+    Total_mass = MassOfNeucleon(Num_proton,Num_neutron)
     print(f"The total mass of neucleon is: {Total_mass} amu")
