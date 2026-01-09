a4d009bef4ea6da2b139e4c1b85cd86d378a1da8
Krish-01-code <krishkushwaha.w@gmail.com>
2026-01-08 23:57:59 -0500

    function3

---
+++ Python32-function3.py
@@ -0,0 +1,7 @@
+#Keyword function:
+# It helps with readability of the code
+
+def greetings(hello, first_name, last_name):
+    print(f"{hello}, {first_name} {last_name}")
+
+greetings("Heyy", first_name = "Krish", last_name = "Kushwaha",)
